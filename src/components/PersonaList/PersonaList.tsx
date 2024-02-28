aaabac2964fe564eff57464e20660fe8cee3fb3a
Felipe Vogt <felipevogt.f@gmail.com>
2024-02-27 22:20:05 -0300

    hotfix: Responsive fix and SEO

--- src/components/PersonaList/PersonaList.tsx
+++ src/components/PersonaList/PersonaList.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 import type { Personas } from '@models/persona.model';
 import './PersonaList.css';
 import { getBaseUrl } from '@utils/baseUrl';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 interface PersonaListProps {
   data: Personas;
@@ -10,8 +11,10 @@ interface PersonaListProps {
 export function PersonaList({ data }: PersonaListProps) {
   const [filterText, setFilterText] = useState('');
   const inputRef = useRef<HTMLInputElement>(null);
+  const [hasMore, setHasMore] = useState(true);
+  const [itemsToShow, setItemsToShow] = useState(20); // Mostrar 20 elementos inicialmente
 
-  const handleInputChange = (e: any) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFilterText(e.target.value);
   };
 
@@ -21,6 +24,15 @@ export function PersonaList({ data }: PersonaListProps) {
 
   const handleContainerClick = () => {
     inputRef.current!.focus();
+  };
+
+  const fetchMoreData = () => {
+    // Simplemente aumentamos la cantidad de elementos a mostrar
+    setItemsToShow(prevItems => prevItems + 20); // Por ejemplo, aquí incrementamos en 20 elementos
+    if (itemsToShow >= filteredKeys.length) {
+      // Si ya hemos mostrado todos los elementos, establecemos hasMore en false para detener la carga infinita
+      setHasMore(false);
+    }
   };
 
   return (
@@ -34,44 +46,56 @@ export function PersonaList({ data }: PersonaListProps) {
           ref={inputRef}
           type="text"
           value={filterText}
-          onInput={handleInputChange}
+          onChange={handleInputChange}
           placeholder="Search persona..."
         />
       </div>
 
-      <section
-        style={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
-          gap: '20px',
-          marginTop: '20px',
-          padding: '0 20px',
-        }}
+      <InfiniteScroll
+        dataLength={itemsToShow}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={
+          <div className="loading">
+            <span>Loading...</span>
+          </div>
+        }
       >
-        {filteredKeys.map(key => (
-          <a href={`./persona/${key}`} className="persona-card" key={key}>
-            <div className="persona-card__background">
-              <div className="persona-card__background2"></div>
-            </div>
+        <section
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
+            gap: '20px',
+            marginTop: '20px',
+            padding: '0 20px',
+            overflowY: 'hidden',
+          }}
+        >
+          {filteredKeys.slice(0, itemsToShow).map(key => (
+            <a href={`./persona/${key}`} className="persona-card" key={key}>
+              <div className="persona-card__background">
+                <div className="persona-card__background2"></div>
+              </div>
 
-            <div className="persona-card__content">
-              <span className="persona-card__title">{key}</span>
-              <img
-                src={getBaseUrl() + data[key].img}
-                alt={key}
-                loading="lazy"
-                style={{
-                  boxSizing: 'border-box',
-                  maxWidth: '100%',
-                  height: '200px',
-                  objectFit: 'contain',
-                  aspectRatio: '1/1',
-                }}
-              />
-            </div>
-          </a>
-        ))}
-      </section>
+              <div className="persona-card__content">
+                <span className="persona-card__title">{key}</span>
+                <img
+                  src={getBaseUrl() + data[key].img}
+                  alt={`${key} persona`}
+                  loading="lazy"
+                  style={{
+                    boxSizing: 'border-box',
+                    maxWidth: '100%',
+                    height: '200px',
+                    objectFit: 'contain',
+                    aspectRatio: '1/1',
+                  }}
+                />
+              </div>
+            </a>
+          ))}
+        </section>
+      </InfiniteScroll>
     </>
   );
 }
